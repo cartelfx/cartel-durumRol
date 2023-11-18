4c8f8e8b9b9d6e29e0f0e6418b5b6d1d62687122
cartel <119452317+cartelfx@users.noreply.github.com>
2023-11-18 13:31:25 +0400

    Update presenceUpdate.js

--- presenceUpdate.js
+++ presenceUpdate.js
@@ -1,1 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+  const durum = `münür`
+  const rolID = ``
+  if (newPresence.activities.length && newPresence.activities[0].name === durum) {
+    if (!newPresence.member.roles.cache.some(role => role.id === rolID)) {
+    
+      newPresence.member.roles.add(rolID).catch(console.log(`Kişiye rol verilemedi.`));
+    }
+  }
+});
